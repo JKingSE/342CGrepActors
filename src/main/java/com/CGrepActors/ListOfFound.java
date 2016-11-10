924f8297dfa605009863f9d8669ec9977af8ffc5
Norwal <jfk1307@g.rit.edu>
2016-11-10 17:58:58 -0500

    Fixed some issues with copying files

--- src/main/java/com/CGrepActors/ListOfFound.java
+++ src/main/java/com/CGrepActors/ListOfFound.java
@@ -1,3 +1,5 @@
+package com.CGrepActors;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/CGrepActors/Search.java
+++ src/main/java/com/CGrepActors/Search.java
@@ -1,3 +1,7 @@
+package com.CGrepActors;
+
+import com.CGrepActors.ListOfFound;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.FileNotFoundException;
