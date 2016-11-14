7f323582a64bcd89e15ff63c99d3a08542963175
Axiat <nate.ortega95@gmail.com>
2016-11-14 10:40:06 -0500

    - moved the documentation to the onRecieve method and removed the "sendMessage" function

--- src/main/java/com/CGrepActors/CollectionActor.java
+++ src/main/java/com/CGrepActors/CollectionActor.java
@@ -16,21 +16,19 @@ public class CollectionActor extends UntypedActor{
         }
     }
 
-    @Override
-    public void onReceive(Object o) throws Throwable {
-
-    }
 
     /***
      * upon receipt, are printed by the CollectionActor. Printout consists of
      * the file name (or "-" for standard input) and the list of matching lines
-     * @param foundObject
      */
-    public void sendMessage(Found foundObject){
+    @Override
+    public void onReceive(Object o) throws Throwable {
 
         /**
          * When all the Found messages have been processed, the CollectionActor
          * shuts down all actors using Actors.registry().shutdown()
          */
     }
+
+
 }
