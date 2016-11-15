e230ca305764f92e119a6d8063a05a2365eba53c
Norwal <jfk1307@g.rit.edu>
2016-11-14 22:42:54 -0500

    Moved old files to a new package

--- src/OldCGrep/ListOfFound.java
+++ src/OldCGrep/ListOfFound.java
@@ -1,4 +1,4 @@
-package CGrepActors;
+package OldCGrep;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/OldCGrep/OldCGrep.java
+++ src/OldCGrep/OldCGrep.java
@@ -1,4 +1,4 @@
-package CGrepActors;
+package OldCGrep;
 
 import java.io.File;
 import java.io.FileNotFoundException;
